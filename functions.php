ad8c8099047319de9f2b31430b3771074efc7691
ozumoo <ozumoo@hotmail.com>
2019-02-05 12:36:36 +0200

    Put code in functions

--- functions.php
+++ functions.php
@@ -5,3 +5,22 @@ function dd($val) {
 	die(var_dump($val));
 	echo "</pre>";
 }
+
+function connectToDb()
+{
+	try {
+		return $pdo = new PDO('mysql:host=127.0.0.1;dbname=todo_simple','root','123456');
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
+}
+
+
+function fetchAllTasks($pdo)
+{
+	$statement = $pdo->prepare('SELECT * FROM todo');
+
+	$statement->execute();
+
+	return $statement->fetchAll(PDO::FETCH_CLASS,'Task');
+}
--- index.php
+++ index.php
@@ -3,17 +3,12 @@ require 'Task.php';
 require 'functions.php';
 
 
-try {
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=todo_simple','root','123456');
-} catch (PDOException $e) {
-	die($e->getMessage());
-}
 
-$statement = $pdo->prepare('SELECT * FROM todo');
 
-$statement->execute();
+$pdo = connectToDb();
 
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
+$tasks = fetchAllTasks($pdo);
+
 
 require 'index.view.php';
 
