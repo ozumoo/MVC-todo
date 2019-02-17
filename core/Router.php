dc238727cb9e89879f25d89cfad7a974030bdbfc
ozumoo <ozumoo@hotmail.com>
2019-02-17 23:48:16 +0200

    refactor router && simple form request

--- core/Router.php
+++ core/Router.php
@@ -2,7 +2,10 @@
 
 class Router
 {
-	protected $routes = [];
+	public $routes = [
+		'GET' => [] ,
+		'POST' => []
+	]; 
 
 	public static function load($file)
 	{
@@ -13,16 +16,22 @@ class Router
 		return $router;
 	}
 
-	public function define($routes)
+
+	public function get($uri , $controller) 
 	{
-		$this->routes = $routes;
+		$this->routes['GET'][$uri] = $controller;
 	}
 
-	public function direct($uri)
+	public function post($uri , $controller) 
+	{
+		$this->routes['POST'][$uri] = $controller;
+	}
+
+	public function direct($uri , $requestType)
 	{  
-		//about/culure --> exists ? so return this route 
-	 	if(array_key_exists($uri, $this->routes)) {
-	 		return $this->routes[$uri];
+		//about/culure --> exists ? so return this controller 
+	 	if(array_key_exists($uri, $this->routes[$requestType])) {
+	 		return $this->routes[$requestType][$uri];
 	 	}
 
 	 	throw new Exception("No route defined ");
--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,10 @@
 <?php require 'views/partials/head.php'; ?> 
-		<h1>Tasks</h1>
 
-		<ul>
-			<?php foreach ($tasks as $task) : ?> 
-				<li>
-					<?php if($task->completed) : ?>
-						<strike><?= $task->description; ?></strike>
-					<?php else: ?> 
-						<?= $task->description; ?> 
-					<?php endif; ?> 
-				</li>
-		    <?php endforeach; ?> 
-		</ul>
+<h1>Submit Your Name</h1>
+
+<form action="names" method="POST">
+	<input name="name">
+	<button type="submit">Submit</button>
+</form>
 <?php require 'views/partials/footer.php'; ?> 
 
